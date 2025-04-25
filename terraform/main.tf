a0169a2081ef1db733965a7a6632ac25b0a911af
Pepukai Hove <pepshove@gmail.com>
2025-04-25 02:28:05 +0200

    fixed by pepu

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,73 +1,52 @@
-# Define IAM Role for ECS Task Execution
+# IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        }
+        },
         Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-# Attach Policy to IAM Role (Replacing managed_policy_arns)
+# Attach ECS Execution Policy to Role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Define ECS Task Definition
+# ECS Cluster
+resource "aws_ecs_cluster" "cluster" {
+  name = var.app_name
+}
+
+# ECS Task Definition
 resource "aws_ecs_task_definition" "task" {
   family                   = var.app_name
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions    = jsonencode([{
-    name          = var.app_name
-    image         = "438465157882.dkr.ecr.af-south-1.amazonaws.com/samas"
-    essential     = true
-    portMappings  = [
-      {
-        containerPort = 3000
-        hostPort      = 3000
-      }
-    ]
+
+  container_definitions = jsonencode([{
+    name         = var.app_name
+    image        = "438465157882.dkr.ecr.af-south-1.amazonaws.com/samas"
+    essential    = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
   }])
 }
 
-# Declare ECS Cluster (if not already declared)
-resource "aws_ecs_cluster" "cluster" {
-  name = var.app_name
-}
-
-# Define ECS Service
-resource "aws_ecs_service" "service" {
-  name            = var.app_name
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  
-  network_configuration {
-    subnets         = var.subnet_ids
-    assign_public_ip = true
-    security_groups = ["sg-0a2625c65e03d0177"]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app_target_group.arn
-    container_name   = var.app_name
-    container_port   = 3000
-  }
-}
-
-# Define Load Balancer (ALB)
+# Application Load Balancer (ALB)
 resource "aws_lb" "app_lb" {
   name               = "${var.app_name}-lb"
   internal           = false
@@ -76,23 +55,56 @@ resource "aws_lb" "app_lb" {
   subnets            = var.subnet_ids
 }
 
-# Define Load Balancer Target Group
+# Target Group for ECS Service
 resource "aws_lb_target_group" "app_target_group" {
   name        = "${var.app_name}-tg"
   port        = 3000
   protocol    = "HTTP"
-  vpc_id      = var.vpc_id
+  vpc_id      = trimspace(var.vpc_id)
   target_type = "ip"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
-# Define Load Balancer Listener
+# Listener for ALB
 resource "aws_lb_listener" "app_listener" {
   load_balancer_arn = aws_lb.app_lb.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
-  
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_target_group.arn
   }
+}
+
+# ECS Service (Fargate)
+resource "aws_ecs_service" "service" {
+  name            = var.app_name
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    assign_public_ip = true
+    security_groups  = ["sg-0a2625c65e03d0177"]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_target_group.arn
+    container_name   = var.app_name
+    container_port   = 3000
+  }
+
+  depends_on = [aws_lb_listener.app_listener]
 }
